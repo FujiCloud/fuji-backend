5e6d9e45292e8ca007d0d07b62fad239ee40aa05
Jack Cook <hello@jackcook.nyc>
2017-08-17 09:04:32 -0700

    Finished porting endpoints to Go

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+    "bytes"
     "database/sql"
     "encoding/json"
     "fmt"
     "net/http"
+    "strconv"
     
     _ "github.com/go-sql-driver/mysql"
 )
@@ -16,7 +18,48 @@ type Event struct {
     Attributes map[string]string
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
+func dashboardHandler(w http.ResponseWriter, r *http.Request) {
+    http.ServeFile(w, r, "dashboard.html")
+}
+
+func dataHandler(w http.ResponseWriter, r *http.Request) {
+    rows, _ := db.Query("SELECT * FROM events WHERE name = 'Content View'")
+    defer rows.Close()
+    
+    pages := make(map[string]int)
+    
+    for rows.Next() {
+        var id int
+        var name string
+        var attributes string
+        rows.Scan(&id, &name, &attributes)
+        
+        eventJson := fmt.Sprintf("{\"name\": \"%s\", \"attributes\": %s}", name, attributes)
+        var event Event
+        json.Unmarshal([]byte(eventJson), &event)
+        page := event.Attributes["page"]
+        
+        if _, ok := pages[page]; ok {
+            pages[page] += 1
+        } else {
+            pages[page] = 1
+        }
+    }
+    
+    var buffer bytes.Buffer
+    buffer.WriteString("page,count\n")
+    
+    for k, v := range pages {
+        buffer.WriteString(k)
+        buffer.WriteString(",")
+        buffer.WriteString(strconv.Itoa(v))
+        buffer.WriteString("\n")
+    }
+    
+    fmt.Fprintf(w, buffer.String())
+}
+
+func eventsHandler(w http.ResponseWriter, r *http.Request) {
     switch r.Method {
     case "POST":
         if api_key, ok := r.Header["Authorization"]; ok {
@@ -50,8 +93,10 @@ func handler(w http.ResponseWriter, r *http.Request) {
 func main() {
     db, _ = sql.Open("mysql", "root:password@tcp(127.0.0.1:3306)/fuji")
     
-    http.HandleFunc("/events", handler)
+    http.HandleFunc("/dashboard", dashboardHandler)
+    http.HandleFunc("/data", dataHandler)
+    http.HandleFunc("/events", eventsHandler)
     
-    fmt.Printf("Running on localhost:8000...")
+    fmt.Printf("Running on localhost:8000...\n")
     http.ListenAndServe(":8000", nil)
 }
