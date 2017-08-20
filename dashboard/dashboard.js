b3fb025c7c26c1263d3fda95c90ed39e558f470e
Jack Cook <hello@jackcook.nyc>
2017-08-20 09:57:27 -0700

    Styled stacked area charts and added a label to the x-axis

--- dashboard/dashboard.js
+++ dashboard/dashboard.js
@@ -1,5 +1,5 @@
 var svg = d3.selectAll("svg"),
-    margin = {top: 20, right: 20, bottom: 30, left: 50},
+    margin = {top: 20, right: 20, bottom: 40, left: 38},
     width = svg.attr("width") - margin.left - margin.right,
     height = svg.attr("height") - margin.top - margin.bottom;
 
@@ -7,7 +7,7 @@ var parseDate = d3.timeParse("%Y %b %d");
 
 var x = d3.scaleTime().range([0, width]),
     y = d3.scaleLinear().range([height, 0]),
-    z = d3.scaleOrdinal(d3.schemeCategory10);
+    z = d3.scaleOrdinal().range(["#393b79", "#5254a3" , "#6b6ecf"]);
 
 var stack = d3.stack();
 
@@ -18,6 +18,16 @@ var area = d3.area()
 
 var g = svg.append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+var xAxisLabel = svg.append("text")
+    .attr("class", "x label")
+    .attr("y", height + 56)
+    .style("fill", "#ffffff")
+    .style("font", "12px sans-serif")
+    .text("Time");
+
+var labelWidth = d3.selectAll(".x.label").nodes()[0].getComputedTextLength();
+xAxisLabel.attr("x", (svg.attr("width") - labelWidth) / 2);
 
 d3.csv("../data", type, function(error, data) {
     if (error) throw error;
@@ -43,6 +53,7 @@ d3.csv("../data", type, function(error, data) {
         .attr("x", width - 6)
         .attr("y", function(d) { return y((d[d.length - 1][0] + d[d.length - 1][1]) / 2); })
         .attr("dy", ".35em")
+        .style("fill", "#ffffff")
         .style("font", "10px sans-serif")
         .style("text-anchor", "end")
         .text(function(d) { return d.key; });
