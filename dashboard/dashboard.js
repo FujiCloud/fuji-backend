8bd4deef6d7f4afb049adf66c7db527b8adff393
Jack Cook <hello@jackcook.nyc>
2017-08-21 22:07:53 -0700

    Remove zero padding on minutes

--- dashboard/dashboard.js
+++ dashboard/dashboard.js
@@ -201,7 +201,7 @@ function generateSessionDuration() {
                     seconds -= 60;
                 }
                 
-                return ("0" + minutes).slice(-2) + ":" + ("0" + seconds).slice(-2)
+                return minutes + ":" + ("0" + seconds).slice(-2)
             }));
     });
 }
